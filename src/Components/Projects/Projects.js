780f62d4a4fc4f3750def0e287dac75f73efa4a4
Majd Tarbin <majd.tarbin@outlook.com>
2019-12-06 21:08:40 -0500

    updated projects view and added legendsapi project

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -3,21 +3,14 @@ import SingleProject from './SingleProject/SingleProject'
 import './Projects.css'
 import projectOne from '../../Media/Projects/rocketball.png'
 import projectTwo from '../../Media/Projects/lolbuilder.png'
+import projectThree from '../../Media/Projects/legendsapi.png'
 
 class Footer extends Component {
     render() {
         return (
             <div className='projects_out'>
             <div className='projects_wrapper'>
-                <SingleProject
-                    img={projectTwo}
-                    title='League of Legends Team Builder'
-                    description='
-                    a React full-stack application based on data from datadragon that allows you to create teams and compare the stats of different compositions.
-                    '
-                    url='https://lolbuilder.netlify.com'
-                    repo='https://github.com/majicmaj/lol-builder'
-                />
+
                 <SingleProject
                     img={projectOne}
                     title='Rocket Ball'
@@ -26,6 +19,24 @@ class Footer extends Component {
                     '
                     url='https://majicmaj.github.io/'
                     repo='https://github.com/majicmaj/rocketball'
+                />
+                <SingleProject
+                    img={projectTwo}
+                    title='Team Builder'
+                    description='
+                    a React full-stack application based on data from datadragon that allows you to create teams and compare the stats of different compositions.
+                    '
+                    url='https://lolbuilder.netlify.com'
+                    repo='https://github.com/majicmaj/lol-builder'
+                />
+                <SingleProject
+                    img={projectThree}
+                    title='LegendsAPI'
+                    description='
+                    Uses the latest NA patch(version) json data and maps summaries out of them to create some data about champions, items, and maps.
+                    '
+                    url='https://lolbuilder.herokuapp.com/'
+                    repo='https://github.com/majicmaj/legendsapi'
                 />
                 </div>
                 </div>
