083a0287169da4e41c5e57e04e6114d2e3f304eb
majicmaj <52975556+majicmaj@users.noreply.github.com>
2020-02-03 14:41:11 -0500

    Changed SSC display picture

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 //import components
-import Nav from './Components/Nav/Nav'
-import RouteManager from './Components/RouteManager/RouteManager'
-import Footer from './Components/Footer/Footer'
- 
+import Nav from "./Components/Nav/Nav";
+import RouteManager from "./Components/RouteManager/RouteManager";
+import Footer from "./Components/Footer/Footer";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Nav />
         <RouteManager />
-        <div className='spacer'/>
+        <div className="spacer" />
         <Footer />
       </div>
-    )
+    );
   }
 }
 
